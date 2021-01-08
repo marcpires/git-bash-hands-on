0c6a6940ee48a5b629049042f20385a4ab5c0842
Alecio Rocha <arocha@daitan.com>
2021-01-08 16:37:14 -0300

    fix: ParameterF correction

--- search.sh
+++ search.sh
@@ -2,26 +2,30 @@
 
 # search.sh
 
-# Script básico que recebe um argumento e procura o arquivo no diretorio HOME.
+#set -X 
 
-# verificando o argumento de entrada
-if [ $# -eq 0 ]; then
-    echo "Faltou utilizar pelo menos 1 argumento!!"
-    exit 1 # Fim do Script
+# Basic script that takes an argument and looks for the file in the HOME directory.
 
-else
-    if [ $# -ne 1 ]; then
-        echo "Você utilizou mais de 1 argumento!!"
-        exit 1 # Fim do Script
+# checking the input argument
+
+function search (){
+   
+if [ -z $FILE ]; then    
+
+        find $HOME  -iname $1      #OBS: You can use "$HOME or ~/" to reference the home location
 
     else
-        read FILE
-        if [ -e $FILE ]; then    
-            find $HOME/ -name $FILE      #OBS: Pode utilizar "$HOME ou ~/" para referenciar o local home
 
-        else
-            echo "Arquivo não encontrado!"
-            exit 1;
-        fi
-    fi    
+        find $HOME  -iname $FILE      #OBS: You can use "$HOME or ~/" to reference the home location
+
 fi
+}
+
+if [ $# -eq 0 ]; then
+    echo "Enter at least one argument: "
+    read FILE
+    search FILE
+else
+    search "$1"
+fi
+
